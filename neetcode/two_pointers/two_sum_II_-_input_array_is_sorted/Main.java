91fb5f0ba7d9ec0a944a4037a8a2b7510703d93c
nonsuch1 <artur.parskov1@gmail.com>
2023-10-28 14:07:47 +0300

    two sum ||

--- neetcode/two_pointers/two_sum_II_-_input_array_is_sorted/Main.java
+++ neetcode/two_pointers/two_sum_II_-_input_array_is_sorted/Main.java
@@ -1,28 +1,39 @@
-class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(new File("input.txt"));
-		int[] nums = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
-		int target = scanner.nextInt();
+import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
 
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(new File("input.txt"));
+		int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int target = scanner.nextInt();
 		Solution solution = new Solution();
-		int[] result = solution.twoSum(nums, target);
-		for (int i: result) {
-			System.out.print(i + " ");
-		}
+		int[] res = solution.twoSum(numbers, target);
+		Arrays.stream(res).forEach(System.out::println);
 	}
 }
 
 class Solution {
 	public int[] twoSum(int[] numbers, int target) {
-		Set<Integer> set = new HashSet<>();
-		for (int num: numbers) {
-			set.add(num);
+		int[] res = new int[2];
+		
+		int left = 0;
+		int right = numbers.length - 1;
+		
+		while (left <= right) {
+			if (numbers[left] + numbers[right] > target) {
+				right--;
+			} else if (numbers[left] + numbers[right] < target) {
+				left++;
+			} else {
+				res[0] = left + 1;
+				res[1] = right + 1;
+				return res;
+			}
 		}
-		int i = 0;
-		while (i < numbers.length) {
-			
-			i++;
-		}
-		return null;
+
+		return res;		
 	}
 }
